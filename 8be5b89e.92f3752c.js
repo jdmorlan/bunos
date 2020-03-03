f81b0a47ff45dc025f689b714ad0e5d4e42a487c
Jay Morlan <jdmorlan@Jays-MacBook-Pro.local>
2020-03-02 21:36:41 -0600

    Deploy website version based on c1e23699c3155e34dd24ee2d0cf049ae35c52113

---
+++ 8be5b89e.92f3752c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{141:function(n){n.exports=JSON.parse('{"bunos":{"allTagsPath":"/blog/tags","slug":"bunos","name":"bunos","count":1,"permalink":"/blog/tags/bunos"}}')}}]);
